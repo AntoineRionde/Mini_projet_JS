9fbbb6b6c9efe1d2828075ee78015805d445f983
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2023-02-03 11:36:39 +0100

    :sparkles: Ajout de la classe Note et NoteView

--- js/notes.js
+++ js/notes.js
@@ -1,3 +1,33 @@
 window.addEventListener('load', () => {
 
 });
+
+function Note(titre, contenu) {
+    this.titre = titre;
+    this.contenu = contenu;
+    this.date = new Date();
+}
+
+let NoteProto = {
+    setTitre(titre) {this.titre = titre;},
+    setContenu(contenu) {this.contenu = contenu;}
+}
+
+Note.prototype = NoteProto;
+
+function NoteView(Note) {
+    this.Note = Note;
+}
+
+let NoteViewProto = {
+    renderHTML() {
+        let conv = new showdown.Converter();
+        return conv.makeHtml(this.Note.contenu);
+    },
+    afficher() {
+        let div = document.querySelector('div#currentNoteView');
+        div.innerHTML = this.renderHTML();
+    }
+}
+
+NoteView.prototype = NoteViewProto;
