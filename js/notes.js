bf5d245c661fd4f5c2c236c9432415d49152f7f0
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2023-02-26 17:55:23 +0100

    Clean all code

--- js/notes.js
+++ js/notes.js
@@ -1,22 +1,23 @@
 window.addEventListener('load', () => {
     GlobalNoteView.init();
-    NoteList.prototype.load();
-    console.log("load");
 });
 
+window.addEventListener("beforeunload", function (e) {
+    GlobalNoteView.noteList.save();
+}, false);
+
 class Note {
-    constructor(titre, contenu) {
-        this.titre = titre;
-        this.contenu = contenu;
-        this.date = new Date();
+    constructor(title, content) {
+        this.title = title;
+        this.content = content;
     }
 
-    setTitre(titre) {
-        this.titre = titre;
+    setTitle(title) {
+        this.title = title;
     }
 
-    setContenu(contenu) {
-        this.contenu = contenu;
+    setContent(content) {
+        this.content = content;
     }
 }
 
@@ -27,32 +28,27 @@ class NoteView {
 
     renderHTML() {
         let conv = new showdown.Converter();
-        return conv.makeHtml(this.note.contenu);
+        return conv.makeHtml("# " + this.note.title + "<hr/>" + this.note.content);
     }
 
-    afficher() {
-        NoteView.visualiserClasse(true);
+    display() {
         let div = document.querySelector('#currentNoteView');
-        div.innerHTML = this.renderHTML();
-    }
-
-    static visualiserClasse(bool) {
-        let div = document.querySelector('#currentNoteView');
-        if (!div.classList.contains('current_note') && bool)
-            div.classList.add('current_note');
-        else if (div.classList.contains('current_note') && !bool)
-            div.classList.remove('current_note');
+        div.innerHTML = this.renderHTML()
+        isAllowedMenu(true);
     }
 }
 
 class NoteList {
+
+    liste = [];
+
     constructor() {
         this.liste = [];
     }
 
     addNote(note) {
         this.liste.push(note);
-        this.save();
+        noteListMenuView.displayItem(note);
         return this.liste.length - 1;
     }
 
@@ -64,38 +60,27 @@ class NoteList {
         return this.liste;
     }
 
-    getIndex()
-    {
-        return this.index;
-    }
-
     save() {
         let value = JSON.stringify(this.liste);
-        localStorage.setItem(GlobalNoteView.indexNoteCourante, value);
+        localStorage.setItem("notes", value)
     }
 
-    load()
-    {
-        let v = localStorage.getItem(GlobalNoteView.indexNoteCourante);
+    load() {
+        let v = localStorage.getItem("notes");
         this.liste = JSON.parse(v);
+        noteListMenuView.update();
     }
 
-    deleteNote(note)
-    {
-        let index = this.liste.indexOf(note);
-        if (index > -1) {
-            this.liste.splice(index, 1);
-        }
+    deleteNote(idNote) {
+        clearInput();
+        this.liste.splice(idNote, 1);
     }
 
-    /* ajouter une méthode pour modifier un élément de la liste de note (classe NoteList). La
-méthode reçoit l'indice de la note modifiée et la nouvelle valeur. Penser à sauvegarder la
-liste dans le localStorage
-     */
-    modifNote(index, newValue)
-    {
-        this.liste.at(index).value = newValue;
-        this.save();
+    editNote(idNote, title, content) {
+        let note = this.liste[idNote];
+        note.setContent(content);
+        note.setTitle(title);
+        noteListMenuView.updateItem(idNote, title);
     }
 }
 
@@ -105,104 +90,184 @@ let noteFormView = {
     },
     hide: function () {
         document.querySelector(".create_edit_note").classList.add("create_edit_note-hidden");
-        NoteView.visualiserClasse(false);
-        clearInput();
     },
     validate: function () {
-        let contenu = document.querySelector("#form_add_note_text").value;
-        let titre = document.querySelector("#form_add_note_title").value;
-        let noteView = new NoteView(new Note(titre, contenu));
-        noteView.afficher();
-        noteListMenuView.displayItem(noteView.note);
-        GlobalNoteView.NoteListe.addNote(noteView.note);
-        this.init();
+        let content = document.querySelector("#form_add_note_text").value;
+        let title = document.querySelector("#form_add_note_title").value;
+        if (content === "" || title === "") {
+            alert("Veuillez remplir tous les champs");
+            return;
+        }
+        let idNote = GlobalNoteView.idcurrentNote;
+        if (idNote === -1) { //ajout
+            idNote = GlobalNoteView.noteList.addNote(new Note(title, content));
+            GlobalNoteView.idcurrentNote = idNote;
+        } else GlobalNoteView.noteList.editNote(idNote, title, content); //modification
+        noteFormView.displayNote();
     },
-
+    
     init: function () {
-        document.querySelector("#noteListMenu").addEventListener("click", this.handler);
+        document.querySelector("#form_add_note_valid").addEventListener("click", this.validate);
     },
-
-    handler: function (e) {
-        let id = function (e) {
-            let id = 0;
-            while (e.previousSibling) {
-                e = e.previousSibling;
-                id++;
-            }
-            return id;
-        }(e.target);
+    
+    //Afficher la note dans la vue
+    displayNote: function () {
+        let note = GlobalNoteView.noteList.getNoteById(GlobalNoteView.idcurrentNote)
+        document.querySelector("#form_add_note_text").value = note.content;
+        document.querySelector("#form_add_note_title").value = note.title;
+        new NoteView(note).display();
+        noteFormView.hide();
+    },
+    
+    //Affiche une alerte si le titre ou le contenu est vide
+    log: function (content, title) {
         
-        let note = GlobalNoteView.NoteListe.getNoteById(id);
-        GlobalNoteView.noteCourante = note;
-        GlobalNoteView.indexNoteCourante = id;
-        GlobalNoteView.changeContent();
-        let noteView = new NoteView(note);
-        
-        noteView.afficher();
     }
 }
 
 let mainMenuView = {
     addHandler: function () {
+        GlobalNoteView.idcurrentNote = -1;
         noteFormView.display();
-        allowedMenu();
+        noteListMenuView.clearSelected();
+        clearInput();
+        isAllowedMenu(false);
     },
-    validateHandler: function () {
-        console.log("validate");
-        noteFormView.validate();
+    editHandler: function (e) {
+        if (e.target.style.cursor === 'pointer') {
+            document.querySelector("#currentNoteView").innerHTML = "";
+            noteFormView.display();
+            isAllowedMenu(false);
+        }
+    },
+    delHandler: function (e) {
+        if (e.target.style.cursor === 'pointer') {
+            let idNote = GlobalNoteView.idcurrentNote;
+            noteListMenuView.deleteItem(idNote);
+        }
     },
     init: function () {
         document.querySelector("#add").addEventListener("click", this.addHandler);
-        document.querySelector("#form_add_note_valid").addEventListener("click", this.validateHandler);
-        document.querySelector("#del").addEventListener("click", noteFormView.hide);
+        document.querySelector("#edit").addEventListener("click", this.editHandler);
+        document.querySelector("#del").addEventListener("click", this.delHandler);
     }
 }
 
 let GlobalNoteView = {
-    noteCourante: new Note,
-    NoteListe: new NoteList,
-    indexNoteCourante: 0,
+    noteList: new NoteList,
+    idcurrentNote: -1,
 
 
     init: function () {
+        GlobalNoteView.noteList.load();
         mainMenuView.init();
+        noteFormView.init();
+        noteListMenuView.init();
         clearInput();
-        notAllowed();
-    },
-    
-    changeContent : function(){
-        document.querySelector("#form_add_note_text").value = GlobalNoteView.noteCourante.contenu;
-        document.querySelector("#form_add_note_title").value = GlobalNoteView.noteCourante.titre;
+        isAllowedMenu(false);
     }
 
 }
 
 let noteListMenuView = {
+
+    init: function () {
+        document.querySelector("#noteListMenu").addEventListener("click", this.handler);
+    },
+
+    update: function () {
+        document.querySelector("#noteListMenu").innerHTML = "";
+        (function () {
+            GlobalNoteView.noteList.getListe().forEach(note => {
+                noteListMenuView.displayItem(note);
+            })
+        })()
+    },
+
     displayItem(note) {
-        let div = document.querySelector('section.note_list');
+        let div = document.querySelector('#noteListMenu');
         let item = document.createElement('div');
         item.classList.add('note_list_item');
-        item.innerHTML = note.titre + " " + note.date.getDay() + "/" + note.date.getMonth() + "/" + note.date.getFullYear();
+        item.innerHTML = note.title + " " + new Date().toISOString().split('T')[0];
         div.appendChild(item);
+    },
+
+    updateItem(idNote, title) {
+        let div = document.querySelector('#noteListMenu');
+        div.childNodes[idNote].innerHTML = title + " " + new Date().toISOString().split('T')[0];
+    },
+
+    deleteItem(idNote) {
+        //supprimer la note de la liste
+        let div = document.querySelector('#noteListMenu');
+        div.removeChild(div.childNodes[idNote]);
+        GlobalNoteView.noteList.deleteNote(idNote);
+
+        //Si la liste est vide
+        if (GlobalNoteView.noteList.getListe().length === 0) {
+            GlobalNoteView.idcurrentNote = -1;
+            clearInput();
+            isAllowedMenu(false);
+            return;
+        }
+        
+        //Selectionner la note précédente ou suivante
+        if (idNote > 0) idNote--;
+
+        //Afficher la note sélectionnée
+        this.handler({target: div.childNodes[idNote] }, idNote);
+    },
+
+    handler: function (e, idNote = -1) {
+        if (!e.target.classList.contains('note_list_item'))
+            return;
+
+        //Récupérer l'idNote de la note cliquée
+        if (idNote === -1) {
+            idNote = function (e) {
+                let id = 0;
+                while (e.previousSibling) {
+                    e = e.previousSibling;
+                    id++;
+                }
+                return id;
+            }(e.target);
+            
+            noteListMenuView.clearSelected();
+        }
+        //Ajouter la classe selected à la note cliquée
+        e.target.classList.add('note_list_item-selected');
+
+        GlobalNoteView.idcurrentNote = idNote;
+        //Afficher la note cliquée
+        noteFormView.displayNote();
+
+    },
+    
+    //Supprimer la classe selected de la note précédente
+    clearSelected: function() {
+        let selected = document.querySelector('.note_list_item-selected');
+        if (selected)
+            selected.classList.remove('note_list_item-selected');
     }
 }
 
+// Efface le contenu des champs de saisie
 function clearInput() {
     document.querySelector("#form_add_note_text").value = "";
     document.querySelector("#form_add_note_title").value = "";
+    document.querySelector("#currentNoteView").innerHTML = "";
 }
 
-function notAllowed() {
-    document.querySelector("#edit").style.cursor = "not-allowed";
-    document.querySelector("#del").style.cursor = "not-allowed";
-    //document.querySelector("#form_add_note_valid").style.cursor = "not-allowed";
-}
-
-function allowedMenu() {
-    document.querySelector("#edit").style.cursor = "pointer";
-    document.querySelector("#del").style.cursor = "pointer";
-}
-
-function allowedValidate() {
-    document.querySelector("#form_add_note_valid").style.cursor = "pointer";
+// Autorise l'édition et la suppresion de la note
+function isAllowedMenu(bool) {
+    let edit = document.querySelector("#edit");
+    let del = document.querySelector("#del");
+    if (!bool) {
+        edit.style.cursor = "not-allowed";
+        del.style.cursor = "not-allowed";
+    } else {
+        edit.style.cursor = "pointer";
+        del.style.cursor = "pointer";
+    }
 }
